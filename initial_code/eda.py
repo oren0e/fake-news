85cfe905cb9b02466a70cf7a9d60acbc771bd382
oren0e <countx@gmail.com>
2020-07-24 21:07:21 +0300

    converetd date string to relevant numeric columns.

--- initial_code/eda.py
+++ initial_code/eda.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
 
+import calendar
+
 pd.set_option('expand_frame_repr', False)  # To view all the variables in the console
 
 # read data
-df_fake: pd.DataFrame = pd.read_csv('../data/Fake.csv')
-df_true: pd.DataFrame = pd.read_csv('../data/True.csv')
+df_fake: pd.DataFrame = pd.read_csv('./data/Fake.csv')
+df_true: pd.DataFrame = pd.read_csv('./data/True.csv')
 
 df_fake.head()
 df_fake.info()
@@ -16,9 +18,9 @@ df_true['label'] = 0
 
 df = pd.concat([df_fake, df_true], axis=0)
 df.head()
-
+del df_true, df_fake
 # shuffle the rows
-df = df.sample(frac=1)
+df = df.sample(frac=1).reset_index()
 df.head()
 
 # how many subjects are there
@@ -29,3 +31,11 @@ df['subject'].value_counts()
 #   2. subjects should be encoded into categories with a missing category as well for unseen categories
 #   3. title and text should participate in the main network procedure.
 
+# fix date
+df['date'] = df['date'].str.rstrip()
+df['month'] = (df['date'].str.extract(r'(^\w+)', expand=True)
+ .replace([month_name for month_name in calendar.month_name], [i for i in range(13)])
+.replace([month_name for month_name in calendar.month_abbr], [i for i in range(13)]))
+df['day'] = df['date'].str.extract(r'(\d+)', expand=True)
+df['year'] = df['date'].str.extract(r'(\d+$)', expand=True)
+df.drop('date', axis=1, inplace=True)
