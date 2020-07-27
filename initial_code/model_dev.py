4bcd4f19c44ad44eaeda0d535f61becdf7d835b5
oren0e <countx@gmail.com>
2020-07-27 19:10:00 +0300

    prepared the sequences to input to the model. quick and dirty for now.

--- initial_code/model_dev.py
+++ initial_code/model_dev.py
@@ -3,7 +3,16 @@ import numpy as np
 
 from typing import Tuple
 
+import string
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
 from sklearn.model_selection import train_test_split
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing import sequence
 
 pd.set_option('expand_frame_repr', False)  # To view all the variables in the console
 
@@ -12,19 +21,45 @@ SEED = 104
 # read the data
 df: pd.DataFrame = pd.read_pickle('./data/df.pkl')
 
+# combine title and text
+df['text'] = df['title'] + ' ' + df['text']
+
 # remove unused variables
-df.drop(['date', 'num_date', 'index'], axis=1, inplace=True)
+df.drop(['date', 'num_date', 'index', 'title'], axis=1, inplace=True)
 
 # train test split
 X_train, X_test, y_train, y_test = train_test_split(df.drop('label', axis=1), df['label'],
                                                     test_size=0.3, random_state=SEED, stratify=df['label'])
 
-def train_val_test_split(X: np.ndarray, y: np.ndarray, test_size: float, val_size: float) -> Tuple[np.ndarray, np.ndarray, np.ndarray,
-                                                                np.ndarray,np.ndarray,np.ndarray]:
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=SEED, stratify=y)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val_size/(1-test_size), random_state=SEED,
-                                                      stratify=y_train)
-    return X_train, X_val, X_test, y_train, y_val, y_test
+# write function to remove punctuation and
+def text_process(text: str) -> str:
+    no_punc = ''.join([word for word in text.rstrip() if word not in string.punctuation]).lower()
+    word_tokens = nltk.word_tokenize(no_punc)
+    # TODO: consider removing the stopwords filter depending on the results of the model
+    no_stopwords = ''.join([word for word in word_tokens if word not in stopwords.words('english')])
 
-X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(df.drop('label', axis=1), df['label'], 0.3, 0.2)
+    return no_stopwords
+
+# find out how many words are in each text
+a = X_train['text'].apply(lambda x: len(x.split(' ')))
+a.describe()    # mean num_words =~ 400, we'll use half of that.
+del a
+
+max_features = 10000
+maxlen = 200
+
+# X_train['text'] = X_train['text'].apply(text_process)   # slow!
+
+# get numbered representation of the corpus
+tokenizer = Tokenizer(num_words=max_features)
+tokenizer.fit_on_texts(X_train['text'].values)
+seqs_train = tokenizer.texts_to_sequences(X_train['text'].values)
+
+tokenizer = Tokenizer(num_words=max_features)
+tokenizer.fit_on_texts(X_test['text'].values)
+seqs_test = tokenizer.texts_to_sequences(X_test['text'].values)
+
+# cut at maxlen words and pad with zeroes if necessary
+seqs_train = sequence.pad_sequences(seqs_train, maxlen=maxlen)
+seqs_test = sequence.pad_sequences(seqs_test, maxlen=maxlen)
 
