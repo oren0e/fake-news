7c473af9f023cda97b41060b1d742147961907fd
oren0e <countx@gmail.com>
2020-08-11 02:04:22 +0300

    add build folder for deploying nodejs to heroku

---
+++ build/precache-manifest.bedf264355b7bee4d9cdd880860e284f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d83337ff0ea75b4594260bd52d1295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23e6f5676a7e5ed6688f",
+    "url": "/static/js/2.12399630.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.12399630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8403649acdc8291b851a",
+    "url": "/static/js/main.ac10bce8.chunk.js"
+  },
+  {
+    "revision": "613777e00798c422250c",
+    "url": "/static/js/runtime-main.0f7ddae7.js"
+  }
+]);
